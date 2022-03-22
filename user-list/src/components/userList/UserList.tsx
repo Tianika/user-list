8471ef9f56a886c8c18d0c1bee919e707962dc58
Tianika <tatyriana@yandex.ru>
2022-03-22 20:19:02 +0300

    feat: add base layout for users list

--- user-list/src/components/userList/UserList.tsx
+++ user-list/src/components/userList/UserList.tsx
@@ -5,8 +5,24 @@ const UserList = () => {
     <div className="user-list">
       <div className="sort-buttons">
         <p className="sort-title">Сортировка</p>
-        <button className="sort-button town">По городу</button>
-        <button className="sort-button company">По компании</button>
+        <button>по городу</button>
+        <button>по компании</button>
+      </div>
+      <div className="user-list-items">
+        <p className="user-list-title">Список пользователей</p>
+        <div className="user-card">
+          <div>
+            ФИО: <span>Имя</span>
+          </div>
+          <div>
+            город: <span>Город</span>
+          </div>
+          <div>
+            компания: <span>компания</span>
+          </div>
+          <button className="detailed">Подробнее</button>
+        </div>
+        <p className="total">Найдено 10 пользователей</p>
       </div>
     </div>
   );
